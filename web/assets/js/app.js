30058c8c23b76429e4f0700b4d5f5a564ddf6277
kmadejski <kamil.madejski@ez.no>
2017-05-24 12:48:16 +0200

    Fix-COM-19992: Bundles issues with Load more button

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -15,4 +15,35 @@ $(document).ready(function(){
         $('.navbar-header').addClass('navbar-show');
         $('#navbar').addClass('navbar-show').addClass('navbar-list-show');
     });
+
+    $("button.load-more").on('click', function() {
+       var url = $(this).data('url');
+       var page = $(this).data('page');
+       var $last = $(this).parent().parent().prev().children().last();
+       var $button = $(this);
+       var $parent = $(this).parent();
+
+       $(this).hide();
+       $parent.append('<div class="button-load-progress"></div>').fadeIn();
+
+        $.ajax({
+            type: 'POST',
+            url: url + '/' + page,
+            dataType: 'json',
+            success: function(data) {
+                if (false === data.showLoadMoreButton) {
+                    $button.parent().empty().hide();
+                }
+                $button.data('page', page + 1);
+                $(data.html).hide().insertAfter($last).fadeIn('slow');
+                $parent.find('div').remove();
+                $button.show();
+            },
+            error: function() {
+                $button.hide();
+                $parent.find('div').remove();
+                $parent.addClass('button-load-error').append('Oh no, something went terribly wrong :-(');
+            }
+        })
+    });
 });
