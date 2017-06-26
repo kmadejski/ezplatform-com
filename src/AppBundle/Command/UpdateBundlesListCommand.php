7fb10021a4f3efbab44e0bcb1f616264cc22f53f
kmadejski <kamil.madejski@ez.no>
2017-06-26 16:02:34 +0200

    Bundle description updating added

--- src/AppBundle/Command/UpdateBundlesListCommand.php
+++ src/AppBundle/Command/UpdateBundlesListCommand.php
@@ -71,7 +71,7 @@ class UpdateBundlesListCommand extends ContainerAwareCommand
                 'forks' => $package['forks'],
             ));
 
-            if ($packageChecksum !== $currentPackage->getFieldValue('checksum')) {
+            if ($packageChecksum !== $currentPackage->getFieldValue('checksum')->__toString()) {
                 $contentInfo = $contentService->loadContentInfo($searchHit->valueObject->versionInfo->contentInfo->id);
                 $contentDraft = $contentService->createContentDraft($contentInfo);
 
@@ -82,6 +82,20 @@ class UpdateBundlesListCommand extends ContainerAwareCommand
                 $contentUpdateStruct->setField('stars', $package['stars']);
                 $contentUpdateStruct->setField('forks', $package['forks']);
                 $contentUpdateStruct->setField('checksum', $packageChecksum);
+
+$xmlText = <<< EOX
+<?xml version='1.0' encoding='utf-8'?>
+<section 
+    xmlns="http://docbook.org/ns/docbook" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    xmlns:ezxhtml="http://ez.no/xmlns/ezpublish/docbook/xhtml" 
+    xmlns:ezcustom="http://ez.no/xmlns/ezpublish/docbook/custom" 
+    version="5.0-variant ezpublish-1.0">
+<para>{$package['description']}</para>
+</section>
+EOX;
+
+                $contentUpdateStruct->setField('description', $xmlText);
 
                 $contentDraft = $contentService->updateContent($contentDraft->versionInfo, $contentUpdateStruct);
                 $content = $contentService->publishVersion($contentDraft->versionInfo);
